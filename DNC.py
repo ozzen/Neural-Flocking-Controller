897cb3d22c61a99000cf364477ad553d7a7ae660
admin <admin@admins-MacBook-Pro.local>
2019-07-23 01:41:04 -0400

    add

--- DNC.py
+++ DNC.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import tensorflow
-import scipy
 import keras
 from keras.layers import Dense
 from keras.models import Sequential
@@ -101,24 +99,6 @@ end = time.perf_counter()
 #output training time
 print(end - start)
 
-# xt = x.loc[[0],:]
-# print(xt)
-# i = 1
-# while i < 335:
-#     test = xt
-#     test = test.values
-#     X_test = sc.fit_transform(test)
-#     y_pred = model.predict(X_test)
-#     for j in range(1,31):
-#         xt['qx' '%d' %(j)] = xt['qx' '%d' %(j)] + 0.1 * y_pred[0,0]
-#         xt['qy' '%d' %(j)] = xt['qy' '%d' %(j)] + 0.1 * y_pred[0,1]
-#         xt['px' '%d' %(j)] = xt['px' '%d' %(j)] + 0.1 * xt['qx' '%d' %(j)]
-#         xt['py' '%d' %(j)] = xt['py' '%d' %(j)] + 0.1 * xt['qy' '%d' %(j)]
-#     i = i + 1
-#
-# print(y_pred.shape)
-#
-# print(xt.head(1))
 
 #training error visualization
 plt.plot(history.history['loss'])
@@ -127,31 +107,69 @@ plt.ylabel('Error')
 plt.xlabel('Epoch')
 plt.show()
 
-#generating accelerations for one agent at each time step
-test = x.head(1)
-test = test.values
-X_test = sc.fit_transform(test)
-start = time.monotonic()
-y_pred = model.predict(X_test)
-end = time.monotonic()
-t = (end - start)
-print(t)
-print(y_pred)
+dft1 = pd.DataFrame()
+dft = pd.DataFrame(columns=['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20',
+                           '21','22','23','24','25','26','27','28','29','30'])
+testdf1 = pd.DataFrame(columns=['px0', 'py0', 'qx0', 'qy0', 'px1', 'py1', 'qx1', 'qy1', 'px2', 'py2', 'qx2', 'qy2',
+                                'px3', 'py3', 'qx3', 'qy3', 'px4', 'py4', 'qx4', 'qy4', 'px5', 'py5', 'qx5', 'qy5'])
+df2 = df.head(1)
+testdf = pd.DataFrame()
+COLUMN_NAMES = df2.columns
+df3 = pd.DataFrame(columns=COLUMN_NAMES)
+steps = 1
+tinput = ['px0', 'px1', 'px2', 'px3', 'px4', 'px5', 'py0', 'py1', 'py2', 'py3', 'py4', 'py5',
+          'qx0', 'qx1', 'qx2', 'qx3', 'qx4', 'qx5', 'qy0', 'qy1', 'qy2', 'qy3', 'qy4', 'qy5']
+
+while steps < 335:
+    for ix in range(1, 31):
+        for jx in range(1, 31):
+            dft1['%d' % (jx)] = np.sqrt((df2['px' '%d' % (ix)] - df2['px' '%d' % (jx)]) ** 2 + (
+                        df2['py' '%d' % (ix)] - df2['py' '%d' % (jx)]) ** 2)
+        dft.loc[dft1.index[0] + ix] = dft1.iloc[0]
+
+    trank = dft.apply(np.argsort, axis=1)
+    tranked_cols = dft.columns.to_series()[trank.values[:, ::1][:, :6]]
+    test_frame = pd.DataFrame(tranked_cols, index=dft.index)
+    for index in range(0, 30):
+        for p in range(0, 6):
+            j = int(test_frame.iloc[index, p])
+            testdf['px' '%d' % (p)] = df2['px' '%d' % (j)]
+            testdf['py' '%d' % (p)] = df2['py' '%d' % (j)]
+            testdf['qx' '%d' % (p)] = df2['qx' '%d' % (j)]
+            testdf['qy' '%d' % (p)] = df2['qy' '%d' % (j)]
+        testdf1.loc[testdf.index[0] + index] = testdf.iloc[0]
+
+    testinput = testdf1[tinput]
+    test = testinput.values
+    X_test = sc.fit_transform(test)
+    y_pred = model.predict(X_test)
+    # print(y_pred)
+
+    for i in range(0, 30):
+        df2['ax' '%d' % (i + 1)] = y_pred[i, 0]
+        df2['ay' '%d' % (i + 1)] = y_pred[i, 1]
+
+    for j in range(1, 31):
+        df2['qx' '%d' % (j)] = df2['qx' '%d' % (j)] + 0.1 * df2['ax' '%d' % (j)]
+        df2['qy' '%d' % (j)] = df2['qy' '%d' % (j)] + 0.1 * df2['ay' '%d' % (j)]
+        df2['px' '%d' % (j)] = df2['px' '%d' % (j)] + 0.1 * df2['qx' '%d' % (j)]
+        df2['py' '%d' % (j)] = df2['py' '%d' % (j)] + 0.1 * df2['qy' '%d' % (j)]
+
+    df3.loc[df2.index[0] + steps] = df2.iloc[0]
+    steps = steps + 1
+print(df3)
 
 #mean squared error calculation
-mse_test = mean_squared_error(y.head(1), y_pred)
-print (mse_test)
-print(y_pred)
-
-model1 = Sequential()
-
-model1.add(LSTM(35, input_shape=(334,24),return_sequences = True, activation='sigmoid'))
-model1.add(LSTM(35,return_sequences=True))
-model1.add(LSTM(2,return_sequences=True))
-#model.add(Dense(2))
-#model.add(Dense(256, activation='relu'))
-#model.add(Dense(2, activation='relu'))
-
-model1.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
-model1.summary()
+# mse_test = mean_squared_error(y.head(1), y_pred)
+# print (mse_test)
+# print(y_pred)
+#
+# model1 = Sequential()
+#
+# model1.add(LSTM(35, input_shape=(334,24),return_sequences = True, activation='sigmoid'))
+# model1.add(LSTM(35,return_sequences=True))
+# model1.add(LSTM(2,return_sequences=True))
+#
+# model1.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
+# model1.summary()
 #history1 = model1.fit(X1, Y1, batch_size=1000, epochs=10000, verbose = 2)
