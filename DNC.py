daea3e55778b969a2f50aa90b0a70aad2531e0a6
admin <admin@admins-MacBook-Pro.local>
2019-07-23 22:37:14 -0400

    neural flocking controller

--- DNC.py
+++ DNC.py
@@ -20,34 +20,34 @@ print(df.shape)
 k = 5
 #total birds
 n = 30
+#time
+dt = 0.1
 
 #calculating Euclidean distance between agents
-df1 = pd.DataFrame()
+eucd_dist = pd.DataFrame()
 for i in range(2,n+1):
-    df1['%d' %(i)] = np.sqrt((df['px1'] - df['px' '%d' %(i)])**2 + (df['py1'] - df['py' '%d' %(i)])**2)
+    eucd_dist['%d' % (i)] = np.sqrt((df['px1'] - df['px' '%d' % (i)]) ** 2 + (df['py1'] - df['py' '%d' % (i)]) ** 2)
 
 #finding the k-nearest neighbors
-arank = df1.apply(np.argsort, axis = 1)
-ranked_cols = df1.columns.to_series()[arank.values[:,::1][:,:k+1]]
-new_frame = pd.DataFrame(ranked_cols, index = df1.index)
-new_frame.columns = ['n1','n2','n3','n4','n5','n6']
-new_frame['n0'] = '1'
-new_frame = new_frame[['n0','n1','n2','n3','n4','n5','n6']]
+arank = eucd_dist.apply(np.argsort, axis = 1)
+ranked_cols = eucd_dist.columns.to_series()[arank.values[:, ::1][:, :k + 1]]
+new_frame = pd.DataFrame(ranked_cols, index = eucd_dist.index)
+new_frame.columns = ['n0','n1','n2','n3','n4','n5']
 
 #collecting final data for training
 l = len(df) - 1
 print(new_frame.head())
-finaldf = pd.DataFrame()
+final_train_df = pd.DataFrame()
 for index in range(0,l):
     for i in range(0,k+1):
         j = int(new_frame.iloc[index,i])
-        finaldf['px' '%d' %(i)] = df['px' '%d' %(j)]
-        finaldf['py' '%d' %(i)] = df['py' '%d' %(j)]
-        finaldf['qx' '%d' %(i)] = df['qx' '%d' %(j)]
-        finaldf['qy' '%d' %(i)] = df['qy' '%d' %(j)]
-        finaldf['ax' '%d' %(i)] = df['ax' '%d' %(j)]
-        finaldf['ay' '%d' %(i)] = df['ay' '%d' %(j)]
-print(finaldf.head())
+        final_train_df['px' '%d' % (i)] = df['px' '%d' % (j)]
+        final_train_df['py' '%d' % (i)] = df['py' '%d' % (j)]
+        final_train_df['qx' '%d' % (i)] = df['qx' '%d' % (j)]
+        final_train_df['qy' '%d' % (i)] = df['qy' '%d' % (j)]
+        final_train_df['ax' '%d' % (i)] = df['ax' '%d' % (j)]
+        final_train_df['ay' '%d' % (i)] = df['ay' '%d' % (j)]
+print(final_train_df.head())
 
 #position and velocities of all the agents
 inputs = ['px0', 'px1', 'px2', 'px3', 'px4', 'px5', 'py0', 'py1', 'py2', 'py3', 'py4', 'py5',
@@ -57,8 +57,8 @@ inputs = ['px0', 'px1', 'px2', 'px3', 'px4', 'px5', 'py0', 'py1', 'py2', 'py3', 
 outputs = ['ax0', 'ay0']
 
 #convert into dataframes
-x = finaldf[inputs]
-y = finaldf[outputs]
+x = final_train_df[inputs]
+y = final_train_df[outputs]
 
 #adjust for NN usage
 X = x.values
@@ -76,10 +76,10 @@ n_cols = X.shape[1]
 
 #hidden layers
 model.add(Dense(64, activation='sigmoid', input_shape=(n_cols,)))
-model.add(Dense(64))
-model.add(Dense(64))
-model.add(Dense(64))
-model.add(Dense(64))
+model.add(Dense(64, activation='sigmoid'))
+model.add(Dense(64, activation='sigmoid'))
+model.add(Dense(64, activation='sigmoid'))
+model.add(Dense(64, activation='sigmoid'))
 
 #output layers
 model.add(Dense(2))
@@ -92,7 +92,7 @@ model.summary()
 start = time.perf_counter()
 
 #training
-history = model.fit(X_train, Y, batch_size = 100 , epochs=1000, verbose = 2)
+history = model.fit(X_train, Y, batch_size = 100 , epochs=100, verbose = 2)
 
 end = time.perf_counter()
 
@@ -107,57 +107,68 @@ plt.ylabel('Error')
 plt.xlabel('Epoch')
 plt.show()
 
-dft1 = pd.DataFrame()
-dft = pd.DataFrame(columns=['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20',
-                           '21','22','23','24','25','26','27','28','29','30'])
-testdf1 = pd.DataFrame(columns=['px0', 'py0', 'qx0', 'qy0', 'px1', 'py1', 'qx1', 'qy1', 'px2', 'py2', 'qx2', 'qy2',
-                                'px3', 'py3', 'qx3', 'qy3', 'px4', 'py4', 'qx4', 'qy4', 'px5', 'py5', 'qx5', 'qy5'])
-df2 = df.head(1)
+##testing phase
+
+#initial configuration for testing
+test_input = df.head(1)
 testdf = pd.DataFrame()
-COLUMN_NAMES = df2.columns
-df3 = pd.DataFrame(columns=COLUMN_NAMES)
+
+#stores distance for knn neighbors for all 30 agents for each time steps
+test_dist = pd.DataFrame()
+step_test_dist = pd.DataFrame(columns=['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15',
+                                       '16', '17', '18', '19', '20', '21','22','23','24','25','26','27','28','29','30'])
+
+#stores data used for testing
+test_final = pd.DataFrame(columns=['px0', 'py0', 'qx0', 'qy0', 'px1', 'py1', 'qx1', 'qy1', 'px2', 'py2', 'qx2', 'qy2',
+                                   'px3', 'py3', 'qx3', 'qy3', 'px4', 'py4', 'qx4', 'qy4', 'px5', 'py5', 'qx5', 'qy5'])
+
+#final dataframe after testing
+COLUMN_NAMES = test_input.columns
+finaldf = pd.DataFrame(columns=COLUMN_NAMES)
+
+# t_input = ['px0', 'px1', 'px2', 'px3', 'px4', 'px5', 'py0', 'py1', 'py2', 'py3', 'py4', 'py5',
+#           'qx0', 'qx1', 'qx2', 'qx3', 'qx4', 'qx5', 'qy0', 'qy1', 'qy2', 'qy3', 'qy4', 'qy5']
+
+#repeat for number of time steps for the neural controller
 steps = 1
-tinput = ['px0', 'px1', 'px2', 'px3', 'px4', 'px5', 'py0', 'py1', 'py2', 'py3', 'py4', 'py5',
-          'qx0', 'qx1', 'qx2', 'qx3', 'qx4', 'qx5', 'qy0', 'qy1', 'qy2', 'qy3', 'qy4', 'qy5']
+while steps < 4:
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            test_dist['%d' % (j)] = np.sqrt((test_input['px' '%d' % (i)] - test_input['px' '%d' % (j)]) ** 2 + (
+                    test_input['py' '%d' % (i)] - test_input['py' '%d' % (j)]) ** 2)
+        step_test_dist.loc[test_dist.index[0] + i] = test_dist.iloc[0]
 
-while steps < 335:
-    for ix in range(1, 31):
-        for jx in range(1, 31):
-            dft1['%d' % (jx)] = np.sqrt((df2['px' '%d' % (ix)] - df2['px' '%d' % (jx)]) ** 2 + (
-                        df2['py' '%d' % (ix)] - df2['py' '%d' % (jx)]) ** 2)
-        dft.loc[dft1.index[0] + ix] = dft1.iloc[0]
-
-    trank = dft.apply(np.argsort, axis=1)
-    tranked_cols = dft.columns.to_series()[trank.values[:, ::1][:, :6]]
-    test_frame = pd.DataFrame(tranked_cols, index=dft.index)
-    for index in range(0, 30):
-        for p in range(0, 6):
+    t_rank = step_test_dist.apply(np.argsort, axis=1)
+    t_ranked_cols = step_test_dist.columns.to_series()[t_rank.values[:, ::1][:, :k + 1]]
+    test_frame = pd.DataFrame(t_ranked_cols, index=step_test_dist.index)
+    for index in range(0, n):
+        for p in range(0, k+1):
             j = int(test_frame.iloc[index, p])
-            testdf['px' '%d' % (p)] = df2['px' '%d' % (j)]
-            testdf['py' '%d' % (p)] = df2['py' '%d' % (j)]
-            testdf['qx' '%d' % (p)] = df2['qx' '%d' % (j)]
-            testdf['qy' '%d' % (p)] = df2['qy' '%d' % (j)]
-        testdf1.loc[testdf.index[0] + index] = testdf.iloc[0]
+            testdf['px' '%d' % (p)] = test_input['px' '%d' % (j)]
+            testdf['py' '%d' % (p)] = test_input['py' '%d' % (j)]
+            testdf['qx' '%d' % (p)] = test_input['qx' '%d' % (j)]
+            testdf['qy' '%d' % (p)] = test_input['qy' '%d' % (j)]
+        test_final.loc[testdf.index[0] + index] = testdf.iloc[0]
 
-    testinput = testdf1[tinput]
+    testinput = test_final[inputs]
     test = testinput.values
     X_test = sc.fit_transform(test)
     y_pred = model.predict(X_test)
     # print(y_pred)
 
-    for i in range(0, 30):
-        df2['ax' '%d' % (i + 1)] = y_pred[i, 0]
-        df2['ay' '%d' % (i + 1)] = y_pred[i, 1]
+    for i in range(0, n):
+        test_input['ax' '%d' % (i + 1)] = y_pred[i, 0]
+        test_input['ay' '%d' % (i + 1)] = y_pred[i, 1]
 
-    for j in range(1, 31):
-        df2['qx' '%d' % (j)] = df2['qx' '%d' % (j)] + 0.1 * df2['ax' '%d' % (j)]
-        df2['qy' '%d' % (j)] = df2['qy' '%d' % (j)] + 0.1 * df2['ay' '%d' % (j)]
-        df2['px' '%d' % (j)] = df2['px' '%d' % (j)] + 0.1 * df2['qx' '%d' % (j)]
-        df2['py' '%d' % (j)] = df2['py' '%d' % (j)] + 0.1 * df2['qy' '%d' % (j)]
+    for j in range(1, n+1):
+        test_input['qx' '%d' % (j)] = test_input['qx' '%d' % (j)] + dt * test_input['ax' '%d' % (j)]
+        test_input['qy' '%d' % (j)] = test_input['qy' '%d' % (j)] + dt * test_input['ay' '%d' % (j)]
+        test_input['px' '%d' % (j)] = test_input['px' '%d' % (j)] + dt * test_input['qx' '%d' % (j)]
+        test_input['py' '%d' % (j)] = test_input['py' '%d' % (j)] + dt * test_input['qy' '%d' % (j)]
 
-    df3.loc[df2.index[0] + steps] = df2.iloc[0]
+    finaldf.loc[test_input.index[0] + steps] = test_input.iloc[0]
     steps = steps + 1
-print(df3)
+print(finaldf)
 
 #mean squared error calculation
 # mse_test = mean_squared_error(y.head(1), y_pred)
